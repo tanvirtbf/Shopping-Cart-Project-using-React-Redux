5c884822752331799f0686edb08e795f0ac4f846
tanvirtbf <tanver018765@gmail.com>
2024-04-10 15:42:05 +0600

    start fresh new project

--- App.js
+++ App.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import Header from './components/Header'
-import { Outlet } from 'react-router-dom'
-
-import './App.css'
 
 export default function App() {
   return (
-    <>
-      <Header />
-      <Outlet />
-    </>
+    <div>
+      App
+    </div>
   )
 }
--- main.js
+++ main.js
@@ -1,35 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import { Provider } from 'react-redux'
-import { store } from './store'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './pages/Home'
-import Cart from './pages/Cart'
-import WishList from './pages/WishList'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: '/cart',
-        element: <Cart />,
-      },
-      {
-        path: '/wishlist',
-        element: <WishList />,
-      },
-    ],
-  },
-])
 
-createRoot(document.querySelector('#root')).render(
-  <Provider store={store}>
-    <RouterProvider router={router} />
-  </Provider>
-)
+createRoot(document.querySelector('#root')).render(<App />)
